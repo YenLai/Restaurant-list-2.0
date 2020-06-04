35ca071f9c5581a3b70df58dd94bc1aaa4bbb3ef
Laisam <b540915@gmail.com>
2020-06-04 14:41:46 +0800

    feat: add register function

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  const { name, email, password } = req.body
+  User.create({
+    name, email, password
+  })
+    .then(() => res.redirect('/user/login'))
+    .catch(err => console.log(err))
 })
 
 
